d1339faf4c4a279f962182e86e470f664cc7e14b
jb2718 <jbattle2718@gmail.com>
2021-08-10 21:55:25 -0500

    added hello app

---
+++ 01_hello_world/program.py
@@ -0,0 +1,8 @@
+print('----------------------------')
+print('         HELLO APP')
+print('----------------------------')
+print()
+
+name = input('What\'s your name? ')
+greeting = "Nice to meet you, " + name + "!"
+print(greeting)
